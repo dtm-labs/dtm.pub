509a5dc6f42ad318faeefc3d97c9a4afccc189d8
yedongfu <dongfuye@163.com>
2021-07-24 16:11:42 +0800

    dir seems ok

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,84 @@
 module.exports = {
-  title: 'DTM',
+  title: 'DTM教程',
   description: 'GO语言分布式事务管理服务',
+  lang: 'zh-CN',
+  head: [['link', { rel: 'icon', type: 'image/jpeg', href: './imgs/dtm.jpeg' }]],
+  themeConfig: {
+    logo: './imgs/dtm.jpeg',
+    sidebar: {
+      '/config/': 'auto',
+      '/plugins': 'auto',
+      // catch-all fallback
+      '/': [
+        {
+          text: '基础',
+          children: [
+            {
+              text: '安装',
+              link: './guide/install'
+            },
+            {
+              text: '开始',
+              link: './guide/start'
+            },
+            {
+              text: '架构',
+              link: './guide/start'
+            }
+          ]
+        },
+        {
+          text: '事务模式',
+          children: [
+            {
+              text: 'TCC',
+              link: './trans/tcc'
+            },
+            {
+              text: 'SAGA',
+              link: './trans/saga'
+            },
+            {
+              text: 'XA',
+              link: './trans/xa'
+            },
+            {
+              text: '事务消息',
+              link: './trans/msg'
+            },
+            {
+              text: '其他',
+              link: './trans/other'
+            }
+          ]
+        },
+        {
+          text: '异常处理',
+          children: [
+            {
+              text: '异常',
+              link: './exception/exception'
+            },
+            {
+              text: '子事务屏障',
+              link: './exception/barrier'
+            }
+          ]
+        },
+        {
+          text: '其他',
+          children: [
+            {
+              text: '其他框架',
+              link: './other/dtrans'
+            },
+            {
+              text: '子事务屏障',
+              link: './exception/barrier'
+            }
+          ]
+        },
+      ]
+    }
+  }
 }
