2b2f0222f181f51d3f5f4acf1951d815a3c3d847
今吾 <20466971@qq.com>
2021-10-12 11:26:35 +0800

    add page bottom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,23 @@
 import Theme from 'vitepress/theme'
+import { h } from 'vue'
 import './custom.css'
 
 export default {
   ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      'page-bottom': () =>
+        h('div', { class: 'footer' }, [
+          h(
+            'a',
+            {
+              href: 'https://beian.miit.gov.cn/',
+              target: '_blank',
+              rel: 'noopener'
+            },
+            [h('span', '京ICP备2021024322号-1')]
+          ),
+        ])
+    })
+  }
 }
