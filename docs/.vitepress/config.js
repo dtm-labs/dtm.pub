bc309061bc0311e5c260158de5b3e282e26b881c
yedf2 <120050102@qq.com>
2022-03-20 09:21:28 +0800

    add kratos

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -183,6 +183,10 @@ module.exports = {
               link: '/ref/gozero'
             },
             {
+              text: 'kratos对接',
+              link: '/ref/kratos'
+            },
+            {
               text: 'HTTP 参考',
               link: '/ref/http'
             },
